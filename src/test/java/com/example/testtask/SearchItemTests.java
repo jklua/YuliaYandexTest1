62c6b09c9edca23d24c7c0e49727fe736dce6efb
jklua <yulia.lukashevich@gmail.com>
2020-10-17 22:00:33 +0300

    Update SearchItemTests.java

--- src/test/java/com/example/testtask/SearchItemTests.java
+++ src/test/java/com/example/testtask/SearchItemTests.java
@@ -1,8 +1,33 @@
 package com.example.testtask;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
 
 public class SearchItemTests {
     WebDriver wd;
+
+    @BeforeClass
+    public void setUp(){
+        wd = new ChromeDriver();
+        wd.manage().window().maximize();
+        wd.navigate().to("https://yandex.ru/");
+
+        wd.findElement(By.cssSelector())
+ /*
+        Перейти на Яндекс Маркет
+4. Выбрать раздел Компьютеры
+5. Выбрать раздел Планшеты
+6. Зайти в расширенный поиск
+7. Задать параметр поиска от 20000 до 35000 рублей.
+8. Выбрать производителя “Apple”
+9. Применить условия поиска
+10. Запомнить второй элемент в результатах поиска
+11. В поисковую строку ввести запомненное значение.
+12. Найти и проверить, что наименование товара соответствует запомненному
+значению.
+         /
+    }
 
 }
